ac33b908c71e9c8269407596228172b70112e06e
sgeldres <sergiogr_@outlook.com>
2021-08-29 00:20:20 -0500

    MatriculaEnLinea

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { InfGeneralComponent } from "./pages/inf-general/inf-general.component";
+import { MatriculaEnLineaComponent } from "./pages/matricula-en-linea/matricula-en-linea.component";
 
 
 const routes: Routes=[
- {path:'infGeneral', component:InfGeneralComponent },
+ {path:'infGeneral', component:InfGeneralComponent},
+ {path:'MatriculaEnLinea', component:MatriculaEnLineaComponent},
  {path: '', redirectTo: 'infGeneral', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { InfGeneralComponent } from './pages/inf-general/inf-general.component';
+import { MatriculaEnLineaComponent } from './pages/matricula-en-linea/matricula-en-linea.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InfGeneralComponent
+    InfGeneralComponent,
+    MatriculaEnLineaComponent
   ],
   imports: [
     BrowserModule,
