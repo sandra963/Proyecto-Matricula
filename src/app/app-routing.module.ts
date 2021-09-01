47dcbda6fe7598eea2e86e98c1a4d38ff2c54ba8
sgeldres <sergiogr_@outlook.com>
2021-09-01 16:45:46 -0500

    Add Matricula-en-linea-consulta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { InfGeneralComponent } from "./pages/inf-general/inf-general.component";
 import { MatriculaEnLineaConfirmacionComponent } from "./pages/matricula-en-linea-confirmacion/matricula-en-linea-confirmacion.component";
 import { MatriculaEnLineaComponent } from "./pages/matricula-en-linea/matricula-en-linea.component";
+import { MatriculaEnLineaConsultaComponent } from "./pages/matricula-en-linea-consulta/matricula-en-linea-consulta.component";
 
 
 
@@ -11,6 +12,7 @@ const routes: Routes=[
  {path:'infGeneral', component:InfGeneralComponent},
  {path:'MatriculaEnLinea', component:MatriculaEnLineaComponent},
  {path:'MatriculaEnLineaConfirmacion', component:MatriculaEnLineaConfirmacionComponent},
+ {path:'MatriculaEnLineaConsulta', component:MatriculaEnLineaConsultaComponent},
  {path: '', redirectTo: 'infGeneral', pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,16 @@ import { AppComponent } from './app.component';
 import { InfGeneralComponent } from './pages/inf-general/inf-general.component';
 import { MatriculaEnLineaComponent } from './pages/matricula-en-linea/matricula-en-linea.component';
 import { MatriculaEnLineaConfirmacionComponent } from './pages/matricula-en-linea-confirmacion/matricula-en-linea-confirmacion.component';
+import { MatriculaEnLineaConsultaComponent } from './pages/matricula-en-linea-consulta/matricula-en-linea-consulta.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     InfGeneralComponent,
     MatriculaEnLineaComponent,
-    MatriculaEnLineaConfirmacionComponent
+    MatriculaEnLineaConfirmacionComponent,
+    MatriculaEnLineaConsultaComponent
   ],
   imports: [
     BrowserModule,
